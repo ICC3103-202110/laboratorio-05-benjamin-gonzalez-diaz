fd9bcc95502c87a519be35d74e719c242d565312
benjamin gonzalez <bigonzalez@miuandes.cl>
2021-05-10 14:05:52 -0400

    instalando librerias a todos los archivos js

---
+++ progrma/update.js
@@ -0,0 +1,25 @@
+var figlet = require('figlet');
+ 
+figlet('Hello World!!', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
+
+const chalk = require('chalk');
+
+// style a string
+console.log(chalk.blue('Hello !'));
+const { printTable } = require('console-table-printer');
+
+//Create a table
+const testCases = [
+  { index: 3, text: 'I would like some gelb bananen bitte', value: 100 },
+  { index: 4, text: 'I hope batch update is working', value: 300 },
+];
+
+//print
+printTable(testCases);
